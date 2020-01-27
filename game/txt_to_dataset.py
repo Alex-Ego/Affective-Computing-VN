70653087bb27001d58413c4c7ef87c8a4712de5c
Alex EG <alexeg98@gmail.com>
2020-01-27 17:07:10 -0600

    Downgraded to TensorFlow 2.0, that fixed a lot of compatibility problems the latest release had

---
+++ game/txt_to_dataset.py
@@ -0,0 +1,132 @@
+
+########## First attempt at actually using TensorFlow, let's see how this goes.
+
+#####
+##  Setup
+#####
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import tensorflow_datasets as tfds
+import os
+
+abs_location = os.path.dirname(os.path.abspath(__file__)) # Absolute path leading to the script file
+file_location = "/nndata/test/" # Path where the data text files are
+file_name_list = ["testdata.txt", "traindata.txt"] # The text file names
+
+for file_name in file_name_list:
+    text_dir = tf.keras.utils.get_file(file_name, origin = "file://" + abs_location + file_location + file_name) # The "file://" is very important
+
+parent_dir = os.path.dirname(text_dir)
+
+#####
+##  Labeling data
+#####
+
+def labeler(example, index):
+    return example, tf.cast(index, tf.int64)
+
+labeled_data_sets = []
+
+for i, file_name in enumerate(file_name_list):
+    lines_dataset = tf.data.TextLineDataset(os.path.join(parent_dir, file_name))
+    labeled_dataset = lines_dataset.map(lambda ex: labeler(ex, i))
+    labeled_data_sets.append(labeled_dataset)
+
+### Shuffling into a single dataset
+BUFFER_SIZE = 50000
+BATCH_SIZE = 64
+TAKE_SIZE = 5000
+
+all_labeled_data = labeled_data_sets[0]
+for labeled_dataset in labeled_data_sets[1:]:
+    all_labeled_data = all_labeled_data.concatenate(labeled_dataset)
+  
+all_labeled_data = all_labeled_data.shuffle(
+    BUFFER_SIZE, reshuffle_each_iteration=False)
+
+for ex in all_labeled_data.take(5):
+    print(ex)
+
+#####
+##  Encoding lines as numbers
+#####
+
+###Build vocabulary
+tokenizer = tfds.features.text.Tokenizer()
+
+vocabulary_set = set()
+for text_tensor, _ in all_labeled_data:
+    some_tokens = tokenizer.tokenize(text_tensor.numpy())
+    vocabulary_set.update(some_tokens)
+
+vocab_size = len(vocabulary_set)
+
+###Encode examples
+encoder = tfds.features.text.TokenTextEncoder(vocabulary_set)
+
+example_text = next(iter(all_labeled_data))[0].numpy()
+print(example_text)
+
+encoded_example = encoder.encode(example_text)
+print(encoded_example)
+
+#An encoder function
+def encode(text_tensor, label):
+    encoded_text = encoder.encode(text_tensor.numpy())
+    return encoded_text, label
+
+def encode_map_fn(text, label):
+    return tf.py_function(encode, inp=[text, label], Tout=(tf.int64, tf.int64))
+
+all_encoded_data = all_labeled_data.map(encode_map_fn)
+
+#####
+##  Test and train batches
+#####
+
+train_data = all_encoded_data.skip(TAKE_SIZE).shuffle(BUFFER_SIZE)
+train_data = train_data.padded_batch(BATCH_SIZE, padded_shapes=([-1],[]))
+
+test_data = all_encoded_data.take(TAKE_SIZE)
+test_data = test_data.padded_batch(BATCH_SIZE, padded_shapes=([-1],[]))
+
+sample_text, sample_labels = next(iter(test_data))
+
+sample_text[0], sample_labels[0]
+
+vocab_size += 1
+
+
+#####
+## Model building
+#####
+
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.Embedding(vocab_size, 64))
+
+model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)))
+
+# One or more dense layers.
+# Edit the list in the `for` line to experiment with layer sizes.
+for units in [64, 64]:
+    model.add(tf.keras.layers.Dense(units, activation='relu'))
+
+# Output layer. The first argument is the number of labels.
+model.add(tf.keras.layers.Dense(3, activation='softmax'))
+
+model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+#####
+## Training
+#####
+
+model.fit(train_data, epochs=3, validation_data=test_data)
+
+eval_loss, eval_acc = model.evaluate(test_data)
+
+print('\nEval loss: {:.3f}, Eval accuracy: {:.3f}'.format(eval_loss, eval_acc))
